e6aa994ef3a3910381a6027df94403ac49cdd5ab
pchmiel <pchmiel@cecsolutions.com>
2017-02-03 13:50:25 -0500

    changed order of code

--- app.js
+++ app.js
@@ -9,17 +9,18 @@ server.listen(process.env.PORT || 3000, function()
     console.log('%s listening to %s', server.name, server.url);
 });
 
-server.get('/', restify.serveStatic({
-    directory: __dirname,
-    default: '/index.html'
-}));
-
 
 // Create chat bot
 var connector = new builder.ChatConnector
 ({ appId: '0dc7da38-9c10-4348-b404-0a209b0fe0f6', appPassword: 'iocgVXAm0ZzfPVFBbiH8oyb' });
 var bot = new builder.UniversalBot(connector);
 server.post('/API/Messages', connector.listen());
+
+server.get('/', restify.serveStatic({
+    directory: __dirname,
+    default: '/index.html'
+}));
+
 
 // Create bot dialogs
 bot.dialog('/', function (session) {
