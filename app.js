ebb545e8b62ad950464cb0ad44d326afa349f957
pchmiel <pchmiel@cecsolutions.com>
2017-02-03 10:48:21 -0500

    Added index.html and modified app.js to use web chat

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ server.listen(process.env.PORT || 3000, function()
     console.log('%s listening to %s', server.name, server.url);
 });
 
+server.get('/', restify.serveStatic({
+    directory: __dirname,
+    default: '/index.html'
+}));
+
+
 // Create chat bot
 var connector = new builder.ChatConnector
 ({ appId: '0dc7da38-9c10-4348-b404-0a209b0fe0f6', appPassword: 'iocgVXAm0ZzfPVFBbiH8oyb' });
