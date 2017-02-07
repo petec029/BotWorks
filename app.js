cf99d05c2a770b46faf2f9c870b66fbcb1080c5a
pchmiel <pchmiel@cecsolutions.com>
2017-02-07 09:42:06 -0500

    try using howdy.ai botkit

--- app.js
+++ app.js
@@ -1,4 +1,50 @@
+var Botkit = require('botkit');
+var controller = Botkit.botframeworkbot({
+});
+
+/*
+var bot = controller.spawn({
+        appId: process.env.app_id,
+        appPassword: process.env.app_password
+});
+*/
+
+var bot = controller.spawn({
+        appId: '0dc7da38-9c10-4348-b404-0a209b0fe0f6',
+        appPassword: 'iocgVXAm0ZzfPVFBbiH8oyb'
+});
+
+
+
+// if you are already using Express, you can use your own server instance...
+// see "Use BotKit with an Express web server"
+controller.setupWebserver(process.env.PORT || 3000,function(err,webserver) {
+  controller.createWebhookEndpoints(controller.webserver, bot, function() {
+      console.log('This bot is online!!!');
+  });
+});
+
+// user said hello
+controller.hears(['hello'], 'message_received', function(bot, message) {
+
+    bot.reply(message, 'Hey there.');
+
+});
+
+controller.hears(['cookies'], 'message_received', function(bot, message) {
+
+    bot.startConversation(message, function(err, convo) {
+
+        convo.say('Did someone say cookies!?!!');
+        convo.ask('What is your favorite type of cookie?', function(response, convo) {
+            convo.say('Golly, I love ' + response.text + ' too!!!');
+            convo.next();
+        });
+    });
+});
+
 // Add your requirements
+/*
 var restify = require('restify');
 var builder = require('botbuilder');
 
@@ -26,3 +72,4 @@ server.get('/', restify.serveStatic({
 bot.dialog('/', function (session) {
     session.send("Hello RightAnswers");
 });
+*/
